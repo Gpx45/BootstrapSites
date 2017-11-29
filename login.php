7a223ab9b5c666650fc94f1d42a07ecdfd37df19
Victor Martins <vich.martins@gmail.com>
2017-11-28 23:27:53 -0500

    added

---
+++ login.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html>
+<header>
+<meta charset="utf-8">
+<title>Login In</title>
+<link rel="stylesheet"
+href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css"
+integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb"
+crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="styles\styles.css"/>
+</header>
+
+<body>
+  <div class="container" style="">
+
+    <div class="col-lg-6 container-fluid">
+      <div class="jumbotron">
+
+        <form>
+          <h3>Login</h3><br />
+          <div class="form-group">
+            <input type="text" class="form-control"
+            placeholder="Username">
+
+          </div>
+
+          <div class="form-group">
+            <input type="password" class="form-control"
+            placeholder="Password">
+
+          </div>
+
+          <div class="checkbox">
+            <label>
+            <input type="checkbox">
+              Remenber Me
+            </label>
+          </div>
+
+          <button type="submit" class="btn btn-primary
+          form-control">Login</button>
+
+
+
+        </form>
+
+      </div>
+    </div>
+  </d>
+
+
+    <div class="col-lg-6"></div>
+
+
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+</body>
+
+</html>
